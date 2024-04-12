8212ccb438beee2dee16b4319fef230530fa5ea6
Long Hello <longhello@Longs-Mac-mini.local>
2024-04-12 17:54:12 +0700

    add Need Help

---
+++ Vendevry/NeedHelpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NeedHelpViewController.swift
+//  Vendevry
+//
+//  Created by Long Hello on 12/04/2024.
+//
+
+import UIKit
+
+class NeedHelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
