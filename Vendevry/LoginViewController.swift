2bcaf201804d22f3910cd6d78c602de36faa84b4
Long Hello <longhello@Longs-Mac-mini.local>
2024-04-15 14:38:42 +0700

    Add home

--- Vendevry/LoginViewController.swift
+++ Vendevry/LoginViewController.swift
@@ -13,4 +13,12 @@ class LoginViewController: BaseVC {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func pushProduct(_ sender: Any) {
+        
+        let vc = UIStoryboard(name: "ProductViewController", bundle: nil).instantiateViewController(identifier: "ProductViewController") as! ProductViewController
+       // vc.isTrue = false
+        Presenter.presentViewController(vc, animated: true)
+    }
 }
---
+++ Vendevry/ProductViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductViewController.swift
+//  Vendevry
+//
+//  Created by Long Hello on 15/04/2024.
+//
+
+import UIKit
+
+class ProductViewController: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
